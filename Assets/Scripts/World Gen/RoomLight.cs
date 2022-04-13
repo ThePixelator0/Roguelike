a77a4cce7a8a1c07f15a61c56336ee53e1f5841c
Jordan <jordanjohnswork@gmail.com>
2022-04-12 22:42:09 -0700

    Lighting Update
    Rooms will now have no light from themselves until the player enters the room. Then, it slightly brightens.
    
    Any normal light sources in the room already are unchanged.

---
+++ Assets/Scripts/World Gen/RoomLight.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;  
+
+public class RoomLight : MonoBehaviour
+{
+    // A room's Light will turn on when a player enters
+    public Light2D li;
+    public float target;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player" && !li.enabled) {
+            target = li.intensity;
+            li.intensity = 0f;
+            li.enabled = true;
+        }
+    }
+
+    void Update() {
+        if (target > li.intensity) {
+            li.intensity += Time.deltaTime / 1;
+            if (target < li.intensity) {
+                li.intensity = target;
+            }
+        }  
+    }
+}
