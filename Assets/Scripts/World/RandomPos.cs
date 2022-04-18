cb77403e4b1553ad06dc899439d18c1ae9b37ccf
Jordan <jordanjohnswork@gmail.com>
2022-04-17 17:36:04 -0700

    Added Banner
    Banner is a junk decoration

--- Assets/Scripts/World/RandomPos.cs
+++ Assets/Scripts/World/RandomPos.cs
@@ -9,12 +9,22 @@ public class RandomPos : MonoBehaviour
     [SerializeField]
     private Vector2 upperBounds = new Vector2(5.5f, 5.5f);
 
+    [Space]
+    [SerializeField]
+    private bool coverEnterance = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
         int xpos = (int)Random.Range(lowerBounds.x, upperBounds.x);
         int ypos = (int)Random.Range(lowerBounds.y, upperBounds.y);
+
+        if (!coverEnterance) {
+            while (xpos <= 1 && xpos >= -1) {
+                xpos = (int)Random.Range(lowerBounds.x, upperBounds.x);
+            }
+        }
 
         Vector2 pos = new Vector2(xpos + transform.position.x, ypos + transform.position.y);
         
