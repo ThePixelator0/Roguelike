e983d1e08467384db99b71badaff93e64210e716
Jordan <jordanjohnswork@gmail.com>
2022-04-19 15:20:38 -0700

    Changed Rooms
    Box Room has a box in the top left corner
    
    Boss Room exit now has a predetermined location

--- Assets/Scripts/Enemies/Boss/BossDie.cs
+++ Assets/Scripts/Enemies/Boss/BossDie.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class BossDie : MonoBehaviour
 {
-    public GameObject exit;
-
     public void Die_Boss() {
-        Instantiate(exit, transform.position, Quaternion.identity);
+        GameObject.Find("Exit").SendMessage("ShowExit");
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/World/exit.cs
+++ Assets/Scripts/World/exit.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class exit : MonoBehaviour
 {
+    private bool active = false;
+    [SerializeField]
+    private TilemapRenderer rend;
+    [SerializeField]
+    private BoxCollider2D selfCol;
+
+    void Start() {
+        rend.enabled = false;
+        selfCol.enabled = false;
+    }
+
     void OnTriggerEnter2D(Collider2D col) {
+        if (active == false) {
+            return;
+        }
+
         if (col.tag == "Player") {
             GameObject.FindWithTag("GameController").SendMessage("NextLevel");
         }
+    }
+
+    public void ShowExit() {
+        rend.enabled = true;
+        selfCol.enabled = true;
     }
 }
