bfa682a5520f17c6641c8d40438c86aa47b87dd9
Jordan <jordanjohnswork@gmail.com>
2022-04-28 11:22:54 -0700

    Exit Button
    Added Exit Button
    
    Also undid the change where RoomHider would let you see 2 rooms instead of 1

--- Assets/Scripts/UI/PauseMenu/ResumeButton.cs
+++ Assets/Scripts/UI/PauseMenu/ResumeButton.cs
@@ -5,22 +5,13 @@ using UnityEngine.UI;
 
 public class ResumeButton : MonoBehaviour
 {
-    public bool resume;
-
-    void Update() {
-        if (resume && (Input.GetAxis("Primary") == 0)) {
-            foreach (Transform child in transform.parent) {
-                if (child != transform) {
-                    child.gameObject.SetActive(false);
-                }
-            }
-            Time.timeScale = 1;
-            gameObject.SetActive(false);
-            resume = false;
-        }
-    }
-
     public void Resume() {
-        resume = true;
+        foreach (Transform child in transform.parent) {
+            if (child != transform) {
+                child.gameObject.SetActive(false);
+            }
+        }
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
     }
 }
